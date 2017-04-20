faf76e4306efe32e7409e3069ad630b37fdb73ff
unknown <S0075231@P0TLS595.itgce.caisse-epargne.fr>
2017-04-20 14:41:46 +0200

    correction

--- exemples/feedReader/src/detail/detail-feed-ctrl.js
+++ exemples/feedReader/src/detail/detail-feed-ctrl.js
@@ -1,5 +1,6 @@
 app.controller("detailFeedCtrl", ['$scope', '$routeParams', '$sce',  function ($scope, $routeParams, $sce) {
     $scope.feed = JSON.parse($routeParams.feed);
     //L'utilisation du service $sce permet de lever le blocage de "cross domaine"
-    $scope.currentUrl = $sce.trustAsResourceUrl($scope.feed.url);
+    //La suppression  "https:" permet de régler le problème de "mixed content"
+    $scope.currentUrl = $sce.trustAsResourceUrl($scope.feed.url.replace("http:",""));
 }]);
